188da518a6285a9a32b717319c6ad20afef3d856
yangqi <13507615840@163.com>
2019-01-21 11:02:47 +0800

    超时配置

--- spring-netty-rpc-common/src/main/java/com/spring/netty/common/remote/DefaultFuture.java
+++ spring-netty-rpc-common/src/main/java/com/spring/netty/common/remote/DefaultFuture.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -11,7 +13,7 @@ import java.util.concurrent.locks.ReentrantLock;
 @Slf4j
 public class DefaultFuture {
 
-    private static Map<String, DefaultFuture> futureMap = new ConcurrentHashMap<>();
+    private static final Map<String, DefaultFuture> futureMap = new ConcurrentHashMap<>();
 
     private Lock lock = new ReentrantLock();
 
@@ -19,12 +21,20 @@ public class DefaultFuture {
 
     private NettyResponse response;
 
+    private long created = System.currentTimeMillis();
+
+    /**
+     * 默认超时 10秒
+     */
+    private long timeOut;
+
     private void setResponse(NettyResponse response) {
         this.response = response;
     }
 
-    public DefaultFuture(NettyRequest request) {
+    public DefaultFuture(NettyRequest request, long timeOut) {
 
+        this.timeOut = timeOut;
         futureMap.put(request.getRequestId(), this);
     }
 
@@ -51,7 +61,12 @@ public class DefaultFuture {
         lock.lock();
         try {
             while (!done()) {
-                condition.await();
+                if (System.currentTimeMillis() - created > timeOut) {
+                    response = new NettyResponse();
+                    response.setException(new TimeoutException());
+                    break;
+                }
+                condition.await(timeOut, TimeUnit.MILLISECONDS);
             }
         } catch (Exception e) {
             log.error("获取response 失败 : {}", e);
