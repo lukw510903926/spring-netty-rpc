dce800aa9c353567ba4c21df1c60f3d500dba3cb
yangqi <lkw510903926@163.com>
2019-07-29 23:12:01 +0800

    自动注册

--- spring-netty-rpc-server/src/main/java/com/spring/netty/server/config/StartRunner.java
+++ spring-netty-rpc-server/src/main/java/com/spring/netty/server/config/StartRunner.java
@@ -4,7 +4,6 @@ import com.spring.netty.common.server.Register;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
