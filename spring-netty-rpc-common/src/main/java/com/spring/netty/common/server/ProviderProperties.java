6328f619dc8d333c005ef64563e31b4ee0437ad0
yangqi <2622319737@qq.com>
2019-07-29 20:58:36 +0800

    hashmap 修改为currentHashmap

---
+++ spring-netty-rpc-common/src/main/java/com/spring/netty/common/server/ProviderProperties.java
@@ -0,0 +1,31 @@
+package com.spring.netty.common.server;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties(prefix = "spring.netty.provider")
+public class ProviderProperties {
+
+    /**
+     * 注册中心类型
+     */
+    private String registerType;
+
+    /**
+     * 注册中心IP
+     */
+    private String registerIp;
+
+    /**
+     * 注册中心端口
+     */
+    private Integer registerPort;
+
+    /**
+     * 服务提供端口
+     */
+    private Integer port;
+
+}
